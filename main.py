720c79f0f04c50728a8547ab1f754920f538d235
shivesh safal <shiveshsafal102@gmail.com>
2020-12-20 21:30:15 +0530

    third commit

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ for index,j in df.iterrows():
     img = Image.open('certificate.jpg')
     draw = ImageDraw.Draw(img)
     draw.text(xy=(725,760),text='{}'.format(j['name']),fill=(0,0,0),font=font)
-    img.save('pictures/{}.jpg'.format(j['name']))
+    img.save('{}.jpg'.format(j['name']))
